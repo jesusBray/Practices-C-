a6a3c5818564c7b6d10815e38f16dd8e70465a4e
jesusBray <jesus.brayan.paye@gmail.com>
2018-09-19 00:09:12 -0400

    Example of a calculator

---
+++ practica1/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    double a,b;
+
+    cout << "Digite un numero" << endl;
+    cin >>a;
+
+    cout << "Digite otro numero" << endl;
+    cin >>b ;
+
+    cout <<"La suma es: "<<a+b<<endl;
+    cout <<"La resta es: "<<a-b<<endl;
+    cout <<"La multiplicacion es: "<<a*b<<endl;
+    cout <<"La divicion es: "<<a/b<<endl;
+
+    return 0;
+}
